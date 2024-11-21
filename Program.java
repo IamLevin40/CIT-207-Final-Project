8f5e9fe0da00a98aa2a702497e2eae683cb710af
IamLevin40 <levincabales40@gmail.com>
2024-11-21 15:23:31 +0800

    Initial commit

---
+++ Program.java
@@ -0,0 +1,14 @@
+// Music Library System
+//    Scenario: Organize and play songs.
+//    OOP Design:
+//       Classes: Song, Album, Playlist, User
+//       Inheritance: RockSong, PopSong inherit from Song.
+//       Polymorphism: play() behavior varies by genre.
+//       Encapsulation: Song metadata private.
+//       Abstraction: Song class with abstract getInfo().
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
